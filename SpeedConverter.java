b5e1c777ed4ae171a25f14cddb113cb630d1a73c
SusieYoo <skald0707@gmail.com>
2021-10-29 01:12:07 +0900

    2021.10.29

---
+++ SpeedConverter.java
@@ -0,0 +1,42 @@
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+        printConversion(301.2);
+
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+            if (kilometersPerHour < 0) {
+                return -1;
+
+            } else {
+                return Math.round(kilometersPerHour / 1.609);
+            }
+        }
+
+
+        public static void printConversion(double kilometersPerHour){
+
+            if (kilometersPerHour<0){
+                System.out.println("Invalid Value");
+            }else{
+                long result = toMilesPerHour(kilometersPerHour);
+                System.out.println(kilometersPerHour + " km/h = " + result + " mi/h");
+            }
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
